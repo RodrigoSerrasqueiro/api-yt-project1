8b0159a5a3dbd9d9b36a07148fa830ecbb4d38e5
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-04-28 13:34:39 -0300

    Update videos.repository.ts

--- src/modules/videos/repositories/videos.repository.ts
+++ src/modules/videos/repositories/videos.repository.ts
@@ -5,15 +5,15 @@ import { Request, Response } from 'express';
 class VideoRepository {
   create(request: Request, response: Response){
 
-    const {title, description, user_id, thumbnail} = request.body;
+    const {title, description, user_id, thumbnail, publishedAt} = request.body;
 
     pool.getConnection((err: any, connection: any) => {
 
       connection.query(
 
-        'INSERT INTO videos (video_id, user_id, title, description, thumbnail) VALUES (?,?,?,?,?)',
+        'INSERT INTO videos (video_id, user_id, title, description, thumbnail, publishedAt) VALUES (?,?,?,?,?,?)',
 
-        [uuidv4(), user_id, title, description, thumbnail],
+        [uuidv4(), user_id, title, description, thumbnail, publishedAt],
         
         (error: any, result: any, filds: any) => {
           connection.release();
