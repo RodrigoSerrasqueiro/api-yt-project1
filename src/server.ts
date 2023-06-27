604015fd96aafd4590ab3d85835fc34e7f81daf8
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-06-27 08:18:02 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userRoutes} from './routes/user.routes';
+import { userRoutes } from './routes/user.routes';
 import { videosRoutes } from './routes/videos.routes';
 import { config } from 'dotenv';
 
@@ -9,7 +9,7 @@ config()
 
 const cors = require('cors');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
@@ -21,4 +21,3 @@ app.use(express.json())
 app.use('/user', userRoutes)
 app.use('/videos', videosRoutes)
 
-app.listen(4000)
